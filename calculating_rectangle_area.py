5a1b9d2b88634cff7a170a4937a86c5463aa42f3
aisha1-umar <110667055+aisha1-umar@users.noreply.github.com>
2022-08-05 11:07:40 +0100

    Add files via upload

---
+++ calculating_rectangle_area.py
@@ -0,0 +1,12 @@
+#program to calculate area of a square
+#area = length * length
+length = 12
+area = length * length
+
+#print result
+print(area)
+
+# perimeter = 2 *(length + breadth)
+breadth = 10
+perimeter = 2 *(length + breadth)
+print(perimeter)
---
+++ first class.py
@@ -0,0 +1,24 @@
+#A basic program
+# written by Aisha
+age = 22 #creating a variable
+account_balance = 33000
+# rules for naming variables
+# name is different from Name and NAME
+# cant start any variable with number or special character e.g ^%$*()
+
+
+
+
+
+
+
+house_number = 10
+#camel case
+houseNumber = 12
+#pascal case
+HouseNumber = 14 # used for functions
+
+
+
+
+
